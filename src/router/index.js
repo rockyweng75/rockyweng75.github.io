e8f4f0201917db7d3e21113a4b829df75c4b176a
rocky wong <42104700+rockyweng75@users.noreply.github.com>
2022-05-19 15:27:08 +0800

    add author

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import constantRoutes from "./constantRoutes"
 
 const base = import.meta.env.VITE_BASE
+console.log(base)
 const router = createRouter({
     history: createWebHistory(base),
     // history: createWebHistory('/eForm_Web'),
